156f494cc35dc8820ebcbdd6fd66aa2c69fcf4c2
arnesssr <arnessvideoclips@gmail.com>
2025-04-10 07:00:13 +0300

    feat: update HTML structure, integrate BrowserRouter in App component, and refine Vite config

--- product-management/src/App.tsx
+++ product-management/src/App.tsx
@@ -1,8 +1,14 @@
 import { AppRoutes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/globals.css'
 
 function App() {
-  return <AppRoutes />
+  console.log('App rendering') // Debug log
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
 
 export default App
